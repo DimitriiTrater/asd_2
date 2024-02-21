2d6927cd52036ff2655a391882188a4c63e83ee1
DimitriiTrater <dima1603040@gmail.com>
2024-02-21 13:10:48 +0200

    dummy commit cuz im lazy

--- src/graph.rs
+++ src/graph.rs
@@ -1,10 +1,12 @@
 use crate::matrix::Matrix;
-use std::collections::{HashMap, VecDeque};
-
+use std::collections::{HashMap, HashSet, VecDeque};
+#[derive(Clone)]
 pub struct Graph<Node, TypeOfRelation> {
     pub relations: HashMap<Node, TypeOfRelation>,
 }
+
 impl<Node, TypeOfRelation> Graph<Node, TypeOfRelation> {
+
   pub fn bfs(graph: &Graph<u32, Vec<u32>>, start: u32, visited: &mut Vec<u32>) {
     let mut queue: VecDeque<u32> = VecDeque::new();
     queue.push_back(start);
@@ -19,6 +21,69 @@ impl<Node, TypeOfRelation> Graph<Node, TypeOfRelation> {
         }
       }
     }
+  }
+  
+  pub fn bfs_with_dist(graph: &Graph<u32, Vec<u32>>, start: u32, dist: &mut Vec<u32>) {
+    let mut queue: VecDeque<u32> = VecDeque::new();
+    queue.push_back(start);
+    dist.push(start);
+
+    while !queue.is_empty() {
+      let vertex = queue.pop_front().unwrap();
+      for neg in &graph.relations[&vertex] {
+        if !dist.contains(&neg) {
+            dist.push(neg.clone());
+            queue.push_back(neg.clone());
+        }
+      }
+    }
+  }
+
+  pub fn bfs_from_to(graph: &Graph<u32, Vec<u32>>, from: u32, to: u32) -> Vec<u32> {
+    let mut queue: VecDeque<u32> = VecDeque::new();
+    let mut path:       Vec<u32>        = Vec::new();
+    let mut visited:    Vec<u32>        = Vec::new();
+
+    queue.push_back(from);
+    visited.push(from);
+
+    while !queue.is_empty() {
+      let vertex = queue.pop_front().unwrap();
+
+      if vertex == to {
+        break;
+      }
+
+      for neg in &graph.relations[&vertex] {
+        if !visited.contains(&neg) {
+            visited.push(neg.clone());
+            queue.push_back(neg.clone());
+        }
+      }
+    }
+
+
+    let mut p = to;
+
+    path.push(p);
+    while p != from {
+      p = visited[p as usize];
+      path.push(p)
+    }
+    path.reverse();
+    return path;
+  }
+
+  pub fn find_components(graph: &Graph<u32, Vec<u32>>) -> Vec<u32> {
+    let mut visited = Vec::new();
+    let mut components = Vec::new();
+    for (vertex, _) in &graph.relations {
+      if ! visited.contains(&vertex.clone()) {
+        Graph::<u32, Vec<u32>>::bfs(graph, vertex.clone(), &mut visited);
+        components.push(vertex.clone());
+      }
+    }
+    components
   }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,142 @@
 pub mod graph;
 pub mod matrix;
+use std::{collections::{vec_deque, VecDeque}, ops::Index};
 
 use crate::matrix::get_matrix_from_file;
-use crate::graph::{get_graph_from_matrix, Graph};
 
 const FILE_PATH: &str = "input/graph.txt";
 
+
+fn trace(from: i32, to: i32, v: &Vec<Vec<i32>>) -> Vec<i32> {
+    let mut frontier:   VecDeque<i32>   = VecDeque::new();
+    let mut path:       Vec<i32>        = Vec::new();
+    let mut visited:    Vec<i32>        = Vec::new();
+
+    visited.resize(v.len(), 0xffff);
+
+    frontier.push_front(from);
+    visited[from as usize] = from;
+
+    while !frontier.is_empty() {
+        let p = frontier.pop_front();
+
+        if p.unwrap() == to {
+            break;
+        }
+
+        let nbrs = &v[p.unwrap() as usize];
+
+        for n in nbrs {
+            if visited[*n as usize] == 0xffff {
+                visited[*n as usize] = p.unwrap();
+                frontier.push_back(*n);
+            }
+        }
+    }
+
+    let mut p = to;
+
+    path.push(p);
+
+    while p != from {
+        p = visited[p as usize];
+        path.push(p);
+    }
+
+    path.reverse();
+
+    return path;
+}
+
+fn bfs(graph: Vec<Vec<i32>>, s: Option<i32>) -> Vec<i32>
+{
+  let mut queue = VecDeque::new();
+  queue.push_front(s.unwrap_or(0));
+  let mut dist: Vec<i32> = Vec::new();
+  dist.resize(graph.len(), -1);
+  dist[s.unwrap_or(0) as usize] = 0;
+  while !queue.is_empty() {
+    let vertex = queue.pop_front();
+    for adj in &graph[vertex.unwrap() as usize] {
+      if dist[*adj as usize] == -1 {
+        queue.push_back(adj.clone());
+        dist[*adj as usize] = dist[(vertex.unwrap()) as usize] + 1;
+      }
+    }
+  }
+  return dist;
+}
+
+fn flatten(v: Vec<Vec<i32>>) -> Vec<i32> {
+  let mut res = Vec::new();
+  for ls in v {
+    for el in ls {
+      res.push(el);
+    }
+  }
+  res
+}
+
+fn components(graph: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+  let mut com = Vec::new();
+  let mut vis = Vec::new();
+  let mut prev = bfs(graph.clone(), None);
+  let mut temp = Vec::new();
+  for (i, j) in prev.iter().enumerate() {
+    if *j != -1 {
+      temp.push(i as i32);
+    }
+  }
+  com.push(temp.clone());
+  temp.clear();
+  while prev.contains(&(-1)) {  
+    let mut nx = Option::None;
+    loop {
+      nx = prev.iter().position(|&r| r == -1);
+      match nx {
+        Some(nx) => {
+          if vis.contains(&(nx as i32)) {
+            prev[nx] = 0;
+          } 
+          else 
+          {
+            break;
+          }
+        },
+        None => return com
+      }
+    }
+    
+    let n = match nx {
+      Some(nx) => Some(nx as i32),
+      None => None  
+    };
+    prev = bfs(graph.clone(), n);
+    
+    for (i, j) in prev.iter().enumerate() {
+      if *j != -1 {
+        temp.push(i as i32);
+      }
+    }
+    com.push(temp.clone());
+    temp.clear();
+    vis = flatten(com.clone());
+  }
+  com
+
+}
+
 fn main() {
   let matrix = get_matrix_from_file(FILE_PATH.to_string());
-  let graph = get_graph_from_matrix(matrix);
-  let mut visited = Vec::new();
-  Graph::<u32, Vec<u32>>::bfs(&graph, 3, &mut visited);
-  println!("{:#?}", visited);
+  let mut re: Vec<Vec<i32>> = Vec::new();
+  re.resize(matrix.len(), vec![]);
+  for (i, row) in matrix.iter().enumerate() {
+    for (j, el) in row.iter().enumerate() {
+      if *el == 1 {
+        re[i].push(j as i32);
+      }
+    }
+  }
 
+  println!("{:#?}", components(re));
 }
